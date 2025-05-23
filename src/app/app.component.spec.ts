6dc1f7ed3c82c9cfda1895150f7c857ab2997ec7
jadaunharsh <jadaunharsh92@gmail.com>
2025-05-23 14:55:21 +0530

    Portfolio content added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Hpprofileapp' title`, () => {
+  it(`should have the 'HarshPortfolio' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Hpprofileapp');
+    expect(app.title).toEqual('HarshPortfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Hpprofileapp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, HarshPortfolio');
   });
 });
